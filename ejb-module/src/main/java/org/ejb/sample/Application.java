5e30b54f72d56a46369c4207c7e26a654ecca493
ab29568 <ab29568@L9MKWMQ2.us.ad.wellpoint.com>
2021-09-23 08:55:26 -0400

    Added changes for Ejb Session Beans Bootification transformation

--- ejb-module/src/main/java/org/ejb/sample/Application.java
+++ ejb-module/src/main/java/org/ejb/sample/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication(scanBasePackages = { "org.ejb.sample" })
+@SpringBootApplication()
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String... args) {
